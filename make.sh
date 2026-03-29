79fb7be6d073c93308d8e53d900ead72838c7a9c
timmer <timmerov+git@gmail.com>
2026-03-29 14:23:11 -0700

    use proper make files.

--- make.sh
+++ make.sh
@@ -1,34 +1,10 @@
 #!/bin/bash
 set -e
 
-# directories
-SLIDSC_DIR=compiler
-BUILD_DIR=build
-BIN_DIR=bin
+echo "building slids compiler..."
+make -C compiler/
 
-mkdir -p "$BUILD_DIR" "$BIN_DIR"
+echo "building sample code..."
+make -C sample/
 
-# build the slidsc compiler
-echo "--- building slidsc compiler ---"
-g++ -std=c++17 -Wall \
-    "$SLIDSC_DIR/main.cpp" \
-    "$SLIDSC_DIR/lexer.cpp" \
-    "$SLIDSC_DIR/parser.cpp" \
-    "$SLIDSC_DIR/codegen.cpp" \
-    -o "$BIN_DIR/slidsc"
-echo "slidsc compiler built: $BIN_DIR/slidsc"
-
-# compile hello.sl -> build/hello.ll
-echo "--- compiling hello.sl ---"
-"$BIN_DIR/slidsc" "$SLIDSC_DIR/hello.sl" -o "$BUILD_DIR/hello.ll"
-
-# assemble hello.ll -> build/hello.o
-echo "--- assembling hello.ll ---"
-llc "$BUILD_DIR/hello.ll" -o "$BUILD_DIR/hello.o" --filetype=obj --relocation-model=pic
-
-# link hello.o -> bin/hello
-echo "--- linking hello ---"
-g++ "$BUILD_DIR/hello.o" -o "$BIN_DIR/hello"
-
-echo "--- running hello ---"
-"$BIN_DIR/hello"
+echo "done!"
